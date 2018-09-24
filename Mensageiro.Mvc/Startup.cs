dd93dd57aac5003cd48ed57ee6e0813c34a5443d
VitorNeilAvelino <avelino.vitor@gmail.com>
2018-09-23 21:13:31 -0300

    Add project files.

---
+++ Mensageiro.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mensageiro.Mvc.Startup))]
+namespace Mensageiro.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
