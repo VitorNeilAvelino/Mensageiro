af51690cf2f81f8e22b4f21c629e23de95682f33
VitorNeilAvelino <avelino.vitor@gmail.com>
2018-10-07 20:05:30 -0300

    Adição de projeto MVC

---
+++ Mensageiro.AspNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mensageiro.AspNet.MvcApplication" Language="C#" %>
---
+++ Mensageiro.AspNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mensageiro.AspNet.Startup))]
+namespace Mensageiro.AspNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
