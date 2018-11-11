44e4e0778faa50ddab09bb12a011d432ebe35143
VitorNeilAvelino <avelino.vitor@gmail.com>
2018-11-11 18:29:55 -0200

    Contato selecionado

--- Mensageiro.AspNet/Scripts/Mensageiro/index.js
+++ Mensageiro.AspNet/Scripts/Mensageiro/index.js
@@ -1,5 +1,21 @@
 ﻿const contatosViewModel = {
-    contatos: ko.observableArray() 
+    contatos: ko.observableArray(),
+    exibirDestinatario: function (contato) {
+        contatosViewModel.contatos().map(contato => contato.selecionado(false));
+        contato.selecionado(true);
+
+        destinatarioViewModel.destinatarioId = contato.id;
+        destinatarioViewModel.destinatarioNome = contato.nome;
+
+        const destinatarioDiv = $("#destinatario")[0];
+        ko.cleanNode(destinatarioDiv);
+        ko.applyBindings(destinatarioViewModel, destinatarioDiv);
+    }
+};
+
+const destinatarioViewModel = {
+    destinatarioNome: "",
+    destinatarioId: ""
 };
 
 atualizarContatos();
@@ -11,8 +27,10 @@ function atualizarContatos() {
         url: "/mensageiro/contatos"
     })
         .then(function (response) {
+            response.map(contato => contato.selecionado = ko.observable(false));
+            
             contatosViewModel.contatos(response);
-            ko.applyBindings(contatosViewModel);
+            ko.applyBindings(contatosViewModel, $("#contatosDiv")[0]);
         })
         .catch(function (erro) { /*Tratamento do erro*/ });
 }
@@ -25,3 +43,15 @@ function conectarUsuarioHub() {
 
     connection.start();
 }
+
+//function obterConversa(remetente, destinatario) {
+//    $.ajax({
+//        url: "/mensageiro/conversas",
+//        data: { remetente, destinatario }
+//    })
+//        .then(function (response) {
+//            destinatarioViewModel = response;
+//            ko.applyBindings(destinatarioViewModel);
+//        })
+//        .catch(function (erro) { /*Tratamento do erro*/ });
+//}
